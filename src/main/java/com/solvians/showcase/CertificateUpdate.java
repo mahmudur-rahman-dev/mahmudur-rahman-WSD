3811416e5127c66e35bc3dc5fa9eb24642c57811
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2024-09-21 17:30:08 +0600

    implement CertificateUpdate class

--- src/main/java/com/solvians/showcase/CertificateUpdate.java
+++ src/main/java/com/solvians/showcase/CertificateUpdate.java
@@ -1,6 +1,26 @@
 package com.solvians.showcase;
 
-public class CertificateUpdate {
+import java.util.concurrent.ThreadLocalRandom;
 
-    // TODO: implement me.
+public class CertificateUpdate {
+    private final long timestamp;
+    private final String isin;
+    private final double bidPrice;
+    private final int bidSize;
+    private final double askPrice;
+    private final int askSize;
+
+   public CertificateUpdate(long timestamp, String isin, double bidPrice, int bidSize, double askPrice, int askSize) {
+        this.timestamp = timestamp;
+        this.isin = isin;
+        this.bidPrice = bidPrice;
+        this.bidSize = bidSize;
+        this.askPrice = askPrice;
+        this.askSize = askSize;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d,%s,%.2f,%d,%.2f,%d", timestamp, isin, bidPrice, bidSize, askPrice, askSize);
+    }
 }
