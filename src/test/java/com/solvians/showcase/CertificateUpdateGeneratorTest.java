434b73997bf023fb1273ae53ca9211b6b67b3c5a
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2024-09-21 18:27:00 +0600

    add unit test for CertificateUpdateGenerator

--- src/test/java/com/solvians/showcase/CertificateUpdateGeneratorTest.java
+++ src/test/java/com/solvians/showcase/CertificateUpdateGeneratorTest.java
@@ -2,19 +2,43 @@ package com.solvians.showcase;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CertificateUpdateGeneratorTest {
 
-    @Test
-    public void generateQuotes() {
-        CertificateUpdateGenerator certificateUpdateGenerator = new CertificateUpdateGenerator(10, 100);
-        Stream<CertificateUpdate> quotes = certificateUpdateGenerator.generateQuotes();
-        assertNotNull(quotes);
-        assertEquals(10 * 100, quotes.count());
+    private final CertificateUpdateGenerator generator = new CertificateUpdateGenerator(1, 1);
 
+    @Test
+    void testGenerateQuote() throws Exception {
+        Callable<String> quoteCallable = generator.generateQuotes().findFirst().orElseThrow();
+        String quote = quoteCallable.call();
+
+        assertNotNull(quote);
+        String[] parts = quote.split(",");
+        assertEquals(6, parts.length);
+
+        assertTrue(Long.parseLong(parts[0]) > 0);
+
+        assertTrue(parts[1].matches("[A-Z]{2}[A-Z0-9]{10}"));
+
+        double bidPrice = Double.parseDouble(parts[2]);
+        assertTrue(bidPrice >= 100.00 && bidPrice <= 200.00);
+
+
+        int bidSize = Integer.parseInt(parts[3]);
+        assertTrue(bidSize >= 1000 && bidSize <= 5000);
+
+
+        double askPrice = Double.parseDouble(parts[4]);
+        assertTrue(askPrice >= 100.00 && askPrice <= 200.00);
+
+
+        int askSize = Integer.parseInt(parts[5]);
+        assertTrue(askSize >= 1000 && askSize <= 10000);
     }
 }
