ddf9c475bf4eea6da1ecbc39e1681ac4b6c9c0e8
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2024-09-21 18:08:55 +0600

    implement CertificateUpdateGenerator and the App

--- src/main/java/com/solvians/showcase/App.java
+++ src/main/java/com/solvians/showcase/App.java
@@ -1,5 +1,12 @@
 package com.solvians.showcase;
 
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
 /**
  * Hello world!
  */
@@ -9,13 +16,35 @@ public class App {
     }
 
     public static void main(String[] args) {
-        if (args.length >= 2) {
-            int threads = Integer.parseInt(args[0]);
-            int quotes = Integer.parseInt(args[1]);
-
-            CertificateUpdateGenerator certificateUpdateGenerator = new CertificateUpdateGenerator(threads, quotes);
-            certificateUpdateGenerator.generateQuotes();
+        if(args.length < 2) {
+            throw new RuntimeException("Expect at least number of threads and number of quotes. But got: " + args);
         }
-        throw new RuntimeException("Expect at least number of threads and number of quotes. But got: " + args);
+
+        int threads = Integer.parseInt(args[0]);
+        int quotes = Integer.parseInt(args[1]);
+
+        var certificateUpdateGenerator = new CertificateUpdateGenerator(threads, quotes);
+        List<Callable<String>> taskList = certificateUpdateGenerator.generateQuotes().collect(Collectors.toList());
+        var executor = Executors.newFixedThreadPool(threads);
+
+        try{
+            List<String> results = executor.invokeAll(taskList)
+                    .stream()
+                    .map(future -> {
+                        try {
+                            return future.get();
+                        } catch (Exception ex) {
+                            throw  new RuntimeException(ex);
+                        }
+                    })
+                    .collect(Collectors.toList());
+            results.forEach(System.out::println);
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("interrupted Thread: " + e.getMessage());
+        } finally {
+            executor.shutdown();
+        }
     }
 }
--- src/main/java/com/solvians/showcase/CertificateUpdateGenerator.java
+++ src/main/java/com/solvians/showcase/CertificateUpdateGenerator.java
@@ -2,25 +2,46 @@ package com.solvians.showcase;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Stream;
 
 public class CertificateUpdateGenerator {
     private final int threads;
     private final int quotes;
+    private final ISINGenerator isinGenerator;
 
     public CertificateUpdateGenerator(int threads, int quotes) {
         this.threads = threads;
         this.quotes = quotes;
+        this.isinGenerator = new ISINGenerator();
     }
 
-    public Stream<CertificateUpdate> generateQuotes() {
-        ThreadLocalRandom random = ThreadLocalRandom.current();
-        // TODO: Implement me.
-        List<CertificateUpdate> updateList = new ArrayList<CertificateUpdate>();
-        for (int i = 0; i < threads * quotes; i++) {
-            updateList.add(new CertificateUpdate());
-        }
-        return Stream.generate(CertificateUpdate::new).parallel().limit(quotes);
+//    public Stream<CertificateUpdate> generateQuotes() {
+//        return Stream.generate(this::generateSingleQuote)
+//                .parallel()
+//                .limit((long) threads * quotes);
+//    }
+
+    public Stream<Callable<String>> generateQuotes() {
+        return Stream.generate(this::createCallable)
+                .limit((long) threads * quotes);
+    }
+
+    public Callable<String> createCallable() {
+        return () -> generateSingleQuote().toString();
+    }
+
+
+    private CertificateUpdate generateSingleQuote() {
+        var threadLocalRandom = ThreadLocalRandom.current();
+        return new CertificateUpdate(
+                System.currentTimeMillis(),
+                isinGenerator.generateISIN(),
+                threadLocalRandom.nextDouble(100.00, 200.01),
+                threadLocalRandom.nextInt(1000, 5001),
+                threadLocalRandom.nextDouble(100.00, 200.01),
+                threadLocalRandom.nextInt(1000, 10001)
+        );
     }
 }
